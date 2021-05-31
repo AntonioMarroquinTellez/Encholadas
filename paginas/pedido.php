c7a9f15c79a5880b735a016c2cafd512bfd5aeeb
AntonioMarroquinTellez <85086905+AntonioMarroquinTellez@users.noreply.github.com>
2021-05-31 17:28:57 -0500

    Add files via upload

---
+++ paginas/pedido.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Formulario</title>
+<link rel="stylesheet" type="text/css" href="pagcss/formulario.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+</head>
+<body>
+<h1><p>Encholadas. <br><p>Pedido Realizado.</p>
+<p>Gracias por comer con nosotros!!</p>
+Se le contactará para confirmar el pedido y el modo de pago.</p>
+<p>Un Sabor Pasado De Verde.</p>
+<button1><a default href="/">Volver al Principio</a></h1></button1>
+</body>
+</html>
---
+++ paginas/reservacion.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Formulario</title>
+<link rel="stylesheet" type="text/css" href="pagcss/formulario.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+</head>
+<body>
+<h1><p>Encholadas. <br><p>Reservación Realizada.</p>
+<p>Gracias por reservar con nosotros!!</p>
+Un sabor pasado de verde.</p>
+<button1><a default href="/">Volver al Principio</a></h1></button1>
+</body>
+</html>
